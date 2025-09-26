755ff58ec9d8b7640ea3f4333975c26c21534e75
yang.yang <Mryang905032390@163.com>
2025-09-26 18:34:19 +0800

    feat(sync): 实现捷安特骑行fit文件上传至顽鹿运动功能
    - 新增批量上传fit文件逻辑
    - 封装上传接口参数，包括token和cookie
    - 添加文件上传计数与响应日志输出
    - 实现上传间隔延时以避免并发问题

---
+++ src/main/java/com/dream/mryang/syncTheRecordingOfOnelapToGiant/UploadToOnelapMain.java
@@ -0,0 +1,61 @@
+package com.dream.mryang.syncTheRecordingOfOnelapToGiant;
+
+import com.dream.mryang.syncTheRecordingOfOnelapToGiant.utils.HttpClientUtil;
+import org.apache.http.Consts;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.StringBody;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author yang.yang
+ * @since 2025/9/26
+ **/
+public class UploadToOnelapMain {
+    /**
+     * 将指定文件上传至顽鹿运动
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // todo 批量下载捷安特骑行fit同步fit文件
+
+        // 计数对象
+        AtomicInteger count = new AtomicInteger(0);
+
+        // fit文件存储路径
+        Path directoryPath = Paths.get("W:\\捷安特fit文件");
+        File directory = directoryPath.toFile();
+        if (directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        // 封装顽鹿运动上传fit文件参数
+                        MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
+                        multipartEntityBuilder.addBinaryBody("jilu", file, ContentType.DEFAULT_BINARY, file.getName());
+                        ContentType CONTENT_TYPE = ContentType.create("text/plain", Consts.UTF_8);
+                        // 封装token
+                        multipartEntityBuilder.addPart("_token", new StringBody("", CONTENT_TYPE));
+
+                        // 封装cookie
+                        HashMap<String, String> headers = new HashMap<>();
+                        headers.put("cookie", "");
+
+                        // 调用接口上传文件
+                        String respondJson = HttpClientUtil.doPostJson("https://u.onelap.cn/upload/fit", null, null,
+                                multipartEntityBuilder, headers);
+                        System.out.println("计数：" + count.incrementAndGet() + "，上传响应值：" + respondJson);
+
+                        // 上传响应生成的文件名称会重复，存在并发问题，需延时！！！
+                        Thread.sleep(2000);
+                    }
+                }
+            }
+        }
+    }
+}
